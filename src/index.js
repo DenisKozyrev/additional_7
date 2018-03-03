d8267a7c93cf5068e89ba9fc2111f822491faa12
Denis Kozyrev <deniskozyrev55@gmail.com>
2018-03-03 22:48:37 +0300

    sudoku solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+
+    let matrixCopy = matrix;
+
+    function changeZero(matrixCopy) {
+        for (let i = 0; i < matrixCopy.length; i++) {
+            for (let j = 0; j < matrixCopy[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrixCopy[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                }
+            }
+        }
+        return matrixCopy;
+    };
+
+    function column(matrixCopy) {
+        for (let i = 0; i < matrixCopy.length; i++) {
+            for (let j = 0; j < matrixCopy[i].length; j++) {
+                if (matrixCopy[i][j]) {
+                    for (let a = 0; a < matrixCopy[i][j].length; a++) {
+                        for (let k = 0; k < 9; k++) {
+                            if (matrixCopy[i][j][k] == matrixCopy[a][j] && !matrixCopy[a][j]) {
+                                matrixCopy[i][j].splice(k, 1);
+                                if (matrixCopy[i][j].length == 1) {
+                                    matrixCopy[i][j] = matrixCopy[i][j][0];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return matrixCopy;
+    };
+
+    function line(matrixCopy) {
+        let arrPos = [];
+        for (let i = 0; i < matrixCopy.length; i++) {
+            for (let j = 0; j < matrixCopy[i].length; j++) {
+                if (matrixCopy[i][j]) {
+                    arrPos.push(j);
+                }
+            }
+            for (let j = 0; j < arrPos.length; j++) {
+                for (let a = 0; a < matrixCopy[i][arrPos[j]].length; a++) {
+                    for (let k = 0; k < matrixCopy[i].length; k++)
+                        if (matrixCopy[i][arrPos[j]][a] == matrixCopy[i][k]) {
+                            matrixCopy[i][arrPos[j]].splice(a, 1);
+                            if (matrixCopy[i][arrPos[j]].length == 1) {
+                                matrixCopy[i][arrPos[j]] = matrixCopy[i][arrPos[j]][0];
+                            }
+                        }
+                }
+            }
+        }
+        return matrixCopy;
+    };
+
+
+    return line(column(changeZero(matrixCopy)));
+};
